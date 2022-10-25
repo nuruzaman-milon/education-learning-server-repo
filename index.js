26f169193e51f2b526f511efd678d6840fca3b72
Njmilon <njmilon1@gmail.com>
2022-10-25 12:23:16 +0600

    find specific course api created

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ app.get('/courses', (req, res) => {
   res.send(courses)
 });
 
+//find a single course using id 
+app.get('/course/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find(n => n._id === id);
+  res.send(selectedCourse);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
