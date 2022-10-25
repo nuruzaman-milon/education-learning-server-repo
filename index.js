7016a07b5055d3aee1be103329c3ca0e639310bd
Njmilon <njmilon1@gmail.com>
2022-10-25 12:21:31 +0600

    create all courses api

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ app.use(cors())
 
 const courses = require('./data/courses.json');
 
+//home route for showing a random message to ui
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+//all courses data api
+app.get('/courses', (req, res) => {
+  res.send(courses)
+});
 
 
 app.listen(port, () => {
